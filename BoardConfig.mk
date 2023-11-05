5ae394e5d069b3ce504574121d98d2e3593a8077
shazu-xd <144534472+shazu-xd@users.noreply.github.com>
2023-11-05 19:58:58 +0530

    fleur: bc: define vendor prop [2/2]
    Rebase soon

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ TARGET_BOARD_PLATFORM := mt6781
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6781
